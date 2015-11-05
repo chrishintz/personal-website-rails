9e15d05dc32be8acf850247ee0b622f204a3ff00
Chris Hintz <chrishintz42@gmail.com>
2015-11-05 10:12:06 -0800

    Center Header Layout

--- personal_website_rails/app/controllers/home_controller.rb
+++ personal_website_rails/app/controllers/home_controller.rb
@@ -1,11 +1,14 @@
 class HomeController < ApplicationController
 
   def index
+    @page = "Chris | Hintz"
   end
 
   def blog
+    @page = "Chris' Blog"
   end
 
   def projects
+    @page = "Chris' Projects"
   end
 end
